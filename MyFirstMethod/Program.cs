665580a760bcb3c1e6ec6e0df96e97a48729ab8e
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-29 11:13:34 +0000

    Adicionei solução ex 1

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,9 +4,24 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Main method, runs all other methods
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountTo10();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Method for counting to 10
+        /// </summary>
+        static void CountTo10()
+        {
+            for(int i = 1 ; i <= 10; i++)
+{
+                Console.WriteLine(i);
+            }
         }
     }
 }
