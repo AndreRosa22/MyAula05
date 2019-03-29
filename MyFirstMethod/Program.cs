0f0a20e29c100cf58c6171f823f6bf9b967b7145
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-29 11:38:34 +0000

    Adicionei solução ex 2

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -10,18 +10,36 @@ namespace MyFirstMethod
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("----CounTo10----");
             CountTo10();
+            Console.WriteLine("----CounToN----");
+            CountToN(10);
+            Console.WriteLine("----CounToN----");
+            CountToN(10);
             Console.ReadKey();
         }
         /// <summary>
-        /// Method for counting to 10
+        /// Method for counting from 1 to 10
         /// </summary>
         static void CountTo10()
         {
-            for(int i = 1 ; i <= 10; i++)
-{
+            for (int i = 1; i <= 10; i++)
+            {
                 Console.WriteLine(i);
             }
         }
+        /// <summary>
+        /// Method for counting from 1 to n
+        /// </summary>
+        /// <param name="n"></param>
+        static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
     }
 }
